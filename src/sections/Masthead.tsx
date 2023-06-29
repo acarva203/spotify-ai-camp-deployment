324d3a50b8f7bea0163a053ceca4de4c1b5846b1
Travis Peng <travispeng@gmail.com>
2023-06-29 00:22:36 -0700

    delete apostrophes

--- src/sections/Masthead.tsx
+++ src/sections/Masthead.tsx
@@ -6,7 +6,7 @@ function Masthead() {
     <header className="masthead pt-28 pb-16 md:pt-64 md:pb-48 text-center text-white relative">
       <div className="container">
         <div className="masthead-subheading text-lg italic leading-6 md:text-4xl md:leading-9 mb-10 font-serif">Welcome to SoundSeeker!</div>
-        <div className="masthead-heading text-6xl font-bold leading-12 md:text-9xl md:leading-15 mb-16 font-sans">It&spos;s Nice To Meet You!</div>
+        <div className="masthead-heading text-6xl font-bold leading-12 md:text-9xl md:leading-15 mb-16 font-sans">Its Nice To Meet You!</div>
         <Link className="btn btn-primary btn-xl uppercase" href="/product">Try out our product</Link>
       </div> 
     </header>
