58d3ddf827092b7a4255813156256be2a5d62dd6
Travis Peng <travispeng@gmail.com>
2023-06-29 00:28:27 -0700

    some UI changes

--- src/sections/Masthead.tsx
+++ src/sections/Masthead.tsx
@@ -7,7 +7,9 @@ function Masthead() {
       <div className="container">
         <div className="masthead-subheading text-lg italic leading-6 md:text-4xl md:leading-9 mb-10 font-serif">Welcome to SoundSeeker!</div>
         <div className="masthead-heading text-6xl font-bold leading-12 md:text-9xl md:leading-15 mb-16 font-sans">Its Nice To Meet You!</div>
-        <Link className="btn btn-primary btn-xl uppercase" href="/product">Try out our product</Link>
+        <Link className="btn btn-primary bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full" href="/product">
+          Try out our product
+        </Link>
       </div> 
     </header>
   );
