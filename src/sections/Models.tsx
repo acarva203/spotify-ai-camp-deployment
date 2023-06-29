389193cdf2760d6ff44d3e2a4f0bbad4754be825
Travis Peng <travispeng@gmail.com>
2023-06-29 16:58:10 -0700

    fix image

--- src/sections/Models.tsx
+++ src/sections/Models.tsx
@@ -24,7 +24,7 @@ function Models() {
         <p className="lead">
           Imagine you have a group of friends who like different types of fruits. Some like apples, some like oranges, and some like bananas. You want to know what fruit a new friend might like based on their preferences. The KNN algorithm works in a similar way. It looks at the preferences of your existing friends (the labeled data) to predict the preference of a new friend (the new input).
         </p>
-        <img src={KNN.src} alt="KNN Model" height={300} />
+        <Image src={KNN} alt="KNN Model" height={300} width={300} />
         <h4 className="my-3">Comprehensive KNN Model Analysis</h4>
         <p className="lead">
           This model had 9% accuracy. As you can see from the confusion matrix, this comprehensive model was terrible and the worst model in predicting song genres. In trying to predict all of the 114 genres correctly, the model predicted less than 1 in 10 correctly.
@@ -34,7 +34,7 @@ function Models() {
         <p className="lead">
           In the condensed model, we grouped the 114 genres into 17 overarching supergenres. This model had approximately 30% accuracy. As you can see from the confusion matrix, the condensed model was better at predicting song genres, yet more often than not predicted the wrong genres.
         </p>
-        <img src={ML2} alt="Condensed KNN Model Analysis" height={500} />
+        <Image src={ML2} alt="Condensed KNN Model Analysis" height={500} width={500} />
         <h3 className="my-3">XG Boost Classifier Model (Extreme Gradient Boosting)</h3>
         <p className="lead">
           XGBoost is like having a team of experts (weak learners) who specialize in different aspects of house affordability. Each expert provides their opinion, and the final decision is made by considering the opinions of all the experts. The team continuously learns from their mistakes and adjusts their opinions to make better predictions. By combining the knowledge of multiple weak models and using an optimization technique called gradient boosting, XGBoost creates a robust and accurate model for classification tasks. It provides insights into the important features driving the predictions, helping you understand the factors that influence the affordability of houses.
