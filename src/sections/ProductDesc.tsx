76b50231a46b2a0eb69bbd76f4d402610f3aff31
Travis Peng <travispeng@gmail.com>
2023-06-29 23:44:09 -0700

    fix apostrophes

--- src/sections/ProductDesc.tsx
+++ src/sections/ProductDesc.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image'
 import flowchart from '@public/flowchart.png';
 
 function ProductDesc() {
   return (
-    <section className="page-section bg-light" id="product-description">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-lg-8">
-            <h2 className="section-heading text-uppercase">Product Description</h2>
-          </div>
+    <section className="py-12" id="product-description">
+      <div className="container mx-auto px-4">
+        <div className="text-center mb-12">
+          <h2 className="text-4xl font-semibold uppercase">Why SoundSeeker?</h2>
         </div>
-        <div className="row justify-content-center">
-          <div className="col-lg-8">
-            <p className="lead"> If you like
-            </p>
-            <li className="lead">discovering new music</li>
-            <li className="lead">curating your playlist</li>
-            <li className="lead">getting personalized recommendations</li>
-            <li className="lead">a fun and interactive experience for all the above</li>
-            <p> </p>
-            <p className="lead">You must try our product, <a href="https://music-reccomender-app-v2.travispeng.repl.co/product">SoundSeeker</a>, available for free.</p>
-            <p className="lead">Benefits: </p>
-            <ol>
-            <li><b>Save Time and Effort</b>: With an extensive library of songs available, it can be time-consuming and overwhelming to manually search for songs that match your preferences. Sound Seeker eliminates the need for manual searching by offering a randomized selection, saving users time and effort in finding music they enjoy.</li>
-              <p> </p>
-            <li><b>Overcome Decision Fatigue</b>: Sometimes, choosing what song to listen to can be challenging, especially when faced with countless options. Sound Seekers randomization feature alleviates decision fatigue by taking the burden of choice off the user, allowing them to sit back, relax, and enjoy the surprise of discovering new tracks</li>
-              <p> </p>
-            <li><b>Broaden Music Taste</b>: Sound Seeker can help users broaden their music taste by exposing them to diverse genres, artists, and musical experiences. It encourages users to step out of their comfort zones and discover new favorites, ultimately enriching their musical journey.</li>
-              </ol>
-            <p className="lead">Flowchart we used to map out processes (Click to see in-depth):</p>
-            <a href="https://lucid.app/lucidspark/09afb384-3035-43ea-bd58-318a4e67ab1d/edit?viewport_loc=-1993%2C-3230%2C2509%2C1375%2C0_0&invitationId=inv_df9d5244-b4ec-49be-b33b-322b0e79226d"><Image src={flowchart} alt="Image" id="myImage" height={500} width={500} /></a>
-
+        <div className="flex justify-center">
+          <div className="lg:w-2/3 text-center">
+            <p className="text-lg mb-6">Imagine a world where...</p>
+            <ul className="list-disc pl-5 mb-6 text-center mx-auto">
+              <li className="text-lg">You&apos;re constantly discovering new music that resonates with your soul</li>
+              <li className="text-lg">Your playlists are always fresh, curated to your unique taste</li>
+              <li className="text-lg">You receive personalized recommendations that hit the mark every time</li>
+              <li className="text-lg">You&apos;re part of a fun and interactive experience that makes all of the above possible</li>
+            </ul>
+            <p className="text-lg mb-6">Welcome to that world. Welcome to <a href="/product" className="text-blue-500">SoundSeeker</a>. And the best part? It&apos;s available for free.</p>
+            <p className="text-xl mb-6 font-bold">Here&apos;s why you&apos;ll love SoundSeeker:</p>
+            <ol className="list-decimal pl-5 mb-6 text-center mx-auto">
+              <li className="text-lg mb-6"><b>Save Time and Effort</b>: With our extensive library of songs, you could spend hours, even days, manually searching for songs that match your preferences. Or you could let SoundSeeker do the work. Our platform offers a randomized selection, saving you time and effort in finding music you&apos;ll love.</li>
+              <li className="text-lg mb-6"><b>Overcome Decision Fatigue</b>: Ever feel overwhelmed by the sheer number of songs to choose from? SoundSeeker&apos;s randomization feature takes the burden of choice off your shoulders. Sit back, relax, and enjoy the thrill of discovering new tracks.</li>
+              <li className="text-lg mb-6"><b>Broaden Your Music Taste</b>: With SoundSeeker, you&apos;ll be exposed to diverse genres, artists, and musical experiences. We encourage you to step out of your comfort zone and discover new favorites. Your musical journey is about to get a lot more exciting.</li>
+            </ol>
+            <p className="text-lg mb-6">Curious about how we do it? Check out the flowchart we used to map out our processes:</p>
+            <div className="flex justify-center">
+              <Link href="https://lucid.app/lucidspark/09afb384-3035-43ea-bd58-318a4e67ab1d/edit?viewport_loc=-1993%2C-3230%2C2509%2C1375%2C0_0&invitationId=inv_df9d5244-b4ec-49be-b33b-322b0e79226d">
+                <Image src={flowchart} alt="Image" id="myImage" height={500} width={500} />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
